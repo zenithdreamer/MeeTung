f4155c2f5e139373487e0cb0a2aaa9790fa33be6
m0owo <8breadsticks8@gmail.com>
2024-10-02 00:13:10 +0700

    fix: set min screen width

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "h-screen bg-background font-sans text-foreground antialiased",
+          "h-screen min-w-[375px] bg-background font-sans text-foreground antialiased",
           GeistSans.variable,
           GeistMono.variable,
         )}
