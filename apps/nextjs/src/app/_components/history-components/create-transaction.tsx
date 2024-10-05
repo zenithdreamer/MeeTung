22f9333ae9764d90e317521b4f79dace5c29784d
m0owo <8breadsticks8@gmail.com>
2024-10-05 22:46:34 +0700

    fix: create transaction button

--- apps/nextjs/src/app/_components/history-components/create-transaction.tsx
+++ apps/nextjs/src/app/_components/history-components/create-transaction.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { AiOutlinePlus } from "@react-icons/all-files/ai/AiOutlinePlus";
 
 export function CreateTransactionButton() {
+  const router = useRouter();
   return (
-    <a
-      href="/transaction/create"
+    <button
+      onClick={() => {
+        router.push("/transaction");
+      }}
       className="absolute bottom-8 right-8 rounded-full bg-[#664F3D] p-4 shadow-md"
     >
       <AiOutlinePlus color="white" size="24px" />
-    </a>
+    </button>
   );
 }
