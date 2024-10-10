3b59ee4e603f0c2735943481fbb50c4fee2a02b6
ZenThaiDev <49715907+ZenThaiDev@users.noreply.github.com>
2024-10-10 15:09:33 +0700

    feat: database schema

--- packages/api/src/router/transaction.ts
+++ packages/api/src/router/transaction.ts
@@ -1,7 +1,8 @@
 import { TRPCError } from "@trpc/server";
 
 import { prisma } from "@mee-tung/db";
-import { createtransactionschema } from "@mee-tung/validators";
+
+//mport { createTransactionSchema } from "@mee-tung/validators";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
 
