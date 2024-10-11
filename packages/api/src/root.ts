728ba7971b1bcf7d6c3de79a086be76559d8daea
Tripong Khemasurijunyo <tripong123@hotmail.com>
2024-10-11 15:52:42 +0700

    fix: oopsie

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,5 +1,6 @@
 import { authRouter } from "./router/auth";
 import { categoryRouter } from "./router/category";
+import { paymentMethodRouter } from "./router/paymentMethod";
 import { transactionRouter } from "./router/transaction";
 import { userRouter } from "./router/user";
 import { createTRPCRouter } from "./trpc";
@@ -9,7 +10,7 @@ export const appRouter = createTRPCRouter({
   user: userRouter,
   transaction: transactionRouter,
   category: categoryRouter,
-  paymentmethod: transactionRouter,
+  paymentmethod: paymentMethodRouter,
 });
 
 // export type definition of API
