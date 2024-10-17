6661b4838d2cc71bd40e76dd4338e0ee2a3edd7f
ZenThaiDev <49715907+ZenThaiDev@users.noreply.github.com>
2024-10-17 10:10:13 +0700

    fix: downgrade argon2

--- apps/backend/next.config.js
+++ apps/backend/next.config.js
@@ -16,7 +16,7 @@ const config = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
